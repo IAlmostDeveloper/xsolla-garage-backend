48404590e04121ff35696d3380f341022569ae4b
rdsalakhov <salakhov.roman12@gmail.com>
2020-09-07 21:18:42 +0500

    added initial db migration

---
+++ migrations/1_initial_migration.down.sql
@@ -0,0 +1,7 @@
+drop table notifications;
+drop table task_media;
+drop table labels;
+drop table subtasks;
+drop table tasks;
+drop table users;
+
---
+++ migrations/1_initial_migration.up.sql
@@ -0,0 +1,67 @@
+CREATE TABLE `users` (
+                         `id` int PRIMARY KEY auto_increment,
+                         `name` varchar(255),
+                         `email` varchar(255),
+                         `country` varchar(255),
+                         `city` varchar(255),
+                         `gender` varchar(255),
+                         `age` int
+);
+
+CREATE TABLE `tasks` (
+                         `id` int PRIMARY KEY auto_increment,
+                         `user_id` int,
+                         `title` varchar(255),
+                         `category` varchar(255),
+                         `text_content` varchar(255),
+                         `date_create` timestamp,
+                         `date_close` timestamp,
+                         `date_target` timestamp,
+                         `is_completed` bool
+);
+
+CREATE TABLE `subtasks` (
+                            `id` int PRIMARY KEY auto_increment,
+                            `task_id` int,
+                            `depth` int,
+                            `index` int,
+                            `title` varchar(255),
+                            `text_content` varchar(255),
+                            `date_create` timestamp,
+                            `date_close` timestamp,
+                            `date_target` timestamp,
+                            `is_completed` bool
+);
+
+CREATE TABLE `notifications` (
+                                 `id` int PRIMARY KEY auto_increment,
+                                 `task_id` int,
+                                 `text_content` varchar(255),
+                                 `notify_time` timestamp,
+                                 `like` int,
+                                 `url` varchar(255),
+                                 `partner` varchar(255)
+);
+
+CREATE TABLE `labels` (
+                          `id` int PRIMARY KEY auto_increment,
+                          `task_id` int,
+                          `name` varchar(255)
+);
+
+CREATE TABLE `task_media` (
+                              `id` int PRIMARY KEY auto_increment,
+                              `task_id` int,
+                              `type` varchar(255),
+                              `media` blob
+);
+
+ALTER TABLE `tasks` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `subtasks` ADD FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`);
+
+ALTER TABLE `notifications` ADD FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`);
+
+ALTER TABLE `labels` ADD FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`);
+
+ALTER TABLE `task_media` ADD FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`);
