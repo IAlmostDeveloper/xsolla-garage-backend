3caec9df947b7b6d5ad5c25b5237ce84338bbca3
rdsalakhov <salakhov.roman12@gmail.com>
2020-09-15 14:24:51 +0500

    removed category field

--- src/dto/task.go
+++ src/dto/task.go
@@ -4,7 +4,6 @@ type Task struct {
 	Id          int     `json:"id" db:"id"`
 	UserId      int     `json:"user_id" db:"user_id"`
 	Title       *string `json:"title" db:"title"`
-	Category    *string `json:"category" db:"category"`
 	TextContent *string `json:"text_content" db:"text_content"`
 	DateCreate  *string `json:"date_create" db:"date_create"`
 	DateClose   *string `json:"date_close" db:"date_close"`
