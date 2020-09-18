e9039f156b6fb28f438bf81623060526482180a8
rdsalakhov <salakhov.roman12@gmail.com>
2020-09-18 17:11:55 +0500

    fixed json decode error

--- src/controllers/errors.go
+++ src/controllers/errors.go
@@ -4,3 +4,4 @@ import "errors"
 
 var errNoTask = errors.New("task not found")
 var errNoChanges = errors.New("no changes")
+var errJsonDecode = errors.New("cannot decode json body")
--- src/controllers/taskController.go
+++ src/controllers/taskController.go
@@ -20,7 +20,10 @@ func NewTaskController(taskService interfaces.TaskServiceProvider) *TaskControll
 
 func (controller *TaskController) CreateTask(writer http.ResponseWriter, request *http.Request) {
 	var task dto.Task
-	json.NewDecoder(request.Body).Decode(&task)
+	if err := json.NewDecoder(request.Body).Decode(&task); err != nil {
+		errorJsonRespond(writer, http.StatusBadRequest, errJsonDecode)
+		return
+	}
 	err := controller.taskService.CreateTask(&task)
 	if err != nil {
 		errorJsonRespond(writer, http.StatusInternalServerError, err)
@@ -69,7 +72,10 @@ func (controller *TaskController) RemoveTaskByID(writer http.ResponseWriter, req
 func (controller *TaskController) UpdateTask(writer http.ResponseWriter, request *http.Request) {
 	taskId, _ := strconv.Atoi(mux.Vars(request)["id"])
 	var task dto.Task
-	json.NewDecoder(request.Body).Decode(&task)
+	if err := json.NewDecoder(request.Body).Decode(&task); err != nil {
+		errorJsonRespond(writer, http.StatusBadRequest, errJsonDecode)
+		return
+	}
 	task.Id = taskId
 	err := controller.taskService.Update(&task)
 	if err != nil {
