97f5ddc121f78217dad03aaebeb269ddfc05cad2
rdsalakhov <salakhov.roman12@gmail.com>
2020-09-22 15:37:12 +0500

    changed varchar data type to text

--- migrations/00003_varchar_fix.sql
+++ migrations/00003_varchar_fix.sql
@@ -4,9 +4,11 @@
 USE XsollaGarage;
 
 ALTER TABLE `tasks` MODIFY `title` TEXT;
+ALTER TABLE `tasks` MODIFY `text_content` TEXT;
 
 -- +goose Down
 ALTER TABLE `tasks` MODIFY `title` VARCHAR(255);
+ALTER TABLE `tasks` MODIFY `text_content` VARCHAR(255);
 
 
 
